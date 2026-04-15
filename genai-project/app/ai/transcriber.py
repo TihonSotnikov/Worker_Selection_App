fd09dcff8372a678a511dbb987483a48248ac6be
Степанов Илья Николаевич <i.stepanov2@g.nsu.ru>
2026-04-15 19:15:35 +0700

    Убрали лишние импорты

--- genai-project/app/ai/transcriber.py
+++ genai-project/app/ai/transcriber.py
@@ -1,6 +1,5 @@
 import torch
-import numpy as np
-from transformers import pipeline
+#УБРАЛ ЛИШНИЕ ИМПОРТЫ
 from faster_whisper import WhisperModel
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
